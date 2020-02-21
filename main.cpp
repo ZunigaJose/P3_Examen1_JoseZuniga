dd68346685e7255c4b2c4c8f815a5d82e3015332
Jose Zuniga <zunigajose@localhost.localdomain>
2020-02-21 15:56:59 -0600

    Mas avanzes en main

--- main.cpp
+++ main.cpp
@@ -28,6 +28,49 @@ Stark nStarks() {
 	return Stark(jefe, animal, lema, lobos, cant);
 }
 
+Lannister nLani() {
+	string jefe, animal, lema, fuerza;
+        int cant, lobos;
+        cout << "Ingrese nombre del jefe: ";
+        getline(cin, jefe);
+	cout << "Ingrese el animal emblema: ";
+        getline(cin, animal);
+	cout << "Ingrese la fuerza de la montaña: ";
+	cin >> fuerza;
+        cout << "Ingrese el lema: ";
+        getline(cin, lema);
+        cout << "Ingrese la cantdad de lobos: ";
+        cin >> lobos;
+        cout << "Ingrese la cantidad de miembros: ";
+        cin >> cant;
+        return Lannister(jefe, animal, lema, fuerza, lobos, cant);
+}
+
+Targaryen nTar() {
+	string jefe, animal, lema;
+        int barcos;
+        cout << "Ingrese nombre de la reina: ";
+        getline(cin, jefe);
+        cout << "Ingrese el animal emblema: ";
+        getline(cin, animal);
+        cout << "Ingrese el lema: ";
+        getline(cin, lema);
+        cout << "Ingrese la cantdad de Barcos: ";
+        cin >> barcos;
+        return Targaryen(jefe, animal, lema, barcos);
+
+}
+
+int menu() {
+	int op;
+	cout << "----Menu-----";
+	cout << "0. Salir\n1. Agregar Familia\n2. Agregar Ejercito\n3. Simular: ";
+	cin >> op;
+	if (op < 0 || op > 3) {
+		return menu();
+	}
+	return op;
+}
 
 int main() {
 	return 0;
